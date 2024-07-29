45790d4dbc22fe0432e39c51c66ba5f30900f6ab
arshdeepkaur001 <143062180+arshdeepkaur001@users.noreply.github.com>
2024-07-29 18:13:43 +0530

    destructuring objects and functions

--- destructuring.js
+++ destructuring.js
@@ -1,77 +1,77 @@
 // DESTRUCTURING
 
-// const obj=
-// {
-//     uid:"arshdeepkaur9926@gmail.com",
-//     pwd:"arsh1234",
-//     server:"localhost",
-//     doConnect:function(pass)
-//     {
-//         if(pass===this.pwd) /// == compares two values with type conversion while === compares two values without type conversion
-//         {
-//             console.log("connected")
-//         }
-//         else
-//         console.log("not connected")
-//     },
-//     doDisconnect:function()
-//     {
-//         console.log("disconnected")
-//     }
-// }
+const obj=
+{
+    uid:"arshdeepkaur9926@gmail.com",
+    pwd:"arsh1234",
+    server:"localhost",
+    doConnect:function(pass)
+    {
+        if(pass===this.pwd) /// == compares two values with type conversion while === compares two values without type conversion
+        {
+            console.log("connected")
+        }
+        else
+        console.log("not connected")
+    },
+    doDisconnect:function()
+    {
+        console.log("disconnected")
+    }
+}
 
-// obj.doConnect("Arsh1234") // === will reject this password as it contains A instead of a
+obj.doConnect("Arsh1234") // === will reject this password as it contains A instead of a
 
-// obj.doConnect("arsh1234") // this password will be accepted
+obj.doConnect("arsh1234") // this password will be accepted
 
-// console.log(obj.uid,obj.pwd) // here we are printing only the uid and pwd from data 
+console.log(obj.uid,obj.pwd) // here we are printing only the uid and pwd from data 
 
-// const {uid, server}=obj // we passed the needed data ie. uid and server to object, this is destructuring
+const {uid, server}=obj // we passed the needed data ie. uid and server to object, this is destructuring
 
-// console.log(uid,server) // now we don't need to write obj. again and again
+console.log(uid,server) // now we don't need to write obj. again and again
 
-// // Destructuring a function and Renaming other variables
+// Destructuring a function and Renaming other variables
 
-// const {uid:id, server:srvr, doDisconnect, doConnect}=obj // original_name : new_name
+const {uid:id, server:srvr, doDisconnect, doConnect}=obj // original_name : new_name
 
-// console.log(id,srvr) 
+console.log(id,srvr) 
 
-// doDisconnect()
+doDisconnect()
 
-// doConnect("arsh1234") // password is correct but output will be "not connected"
+doConnect("arsh1234") // password is correct but output will be "not connected"
 
-// //NOTE- when we take reference of any function while destructuring we get it as an object and it losses the access to its enclosing properties
-// // so it is advised not to destructure any dependent function. Destructure only independent functions.
+//NOTE- when we take reference of any function while destructuring we get it as an object and it losses the access to its enclosing properties
+// so it is advised not to destructure any dependent function. Destructure only independent functions.
 
 
-// //=================================================================
+//=================================================================
 
-// const getAry=()=>{
+const getAry=()=>{
 
-//     return ["arsh","aman","daman","raman"]
+    return ["arsh","aman","daman","raman"]
 
-// }
+}
 
-// const ary=getAry();
+const ary=getAry();
 
-// console.log(ary); // output will be in form of array of strings-  [ 'arsh', 'aman', 'daman', 'raman' ] 
+console.log(ary); // output will be in form of array of strings-  [ 'arsh', 'aman', 'daman', 'raman' ] 
 
-// // but I want only first two names
+// but I want only first two names
 
-// const [n1,n2]=getAry();
+const [n1,n2]=getAry();
 
-// console.log(n1,n2) // output- arsh aman
+console.log(n1,n2) // output- arsh aman
 
-// //similarly
+//similarly
 
-// const names=["arsh","aman","daman","raman"]
+const names=["arsh","aman","daman","raman"]
 
-// const [n3,n4]=names
+const [n3,n4]=names
 
-// console.log(n3,n4)
+console.log(n3,n4)
 
-// // if I want to get the 1st and 3rd name
+// if I want to get the 1st and 3rd name
 
-// const [n5,,n6]=names // an extra comma will skip the 2nd value
+const [n5,,n6]=names // an extra comma will skip the 2nd value
 
-// console.log(n5,n6) // output- arsh daman
+console.log(n5,n6) // output- arsh daman
